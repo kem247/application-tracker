b1233bfcf86f0134be6da00284b821379a1f2a77
June Hosoya <junehosoya@gmail.com>
2020-03-09 20:25:24 -0400

    Create Comment Model and model relations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,7 +2,7 @@ const User = require('./user')
 const Form = require('./form')
 const Question = require('./question')
 const Response = require('./response')
-
+const Comment = require('./comment')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -16,9 +16,20 @@ const Response = require('./response')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+Question.belongsTo(Form)
+Form.hasMany(Question)
+
+Response.belongsTo(Form)
+Form.hasMany(Response)
+
+Comment.belongsTo(User)
+User.hasMany(Comment)
+
 module.exports = {
   User,
   Form,
   Question,
-  Response
+  Response,
+  Comment
 }
