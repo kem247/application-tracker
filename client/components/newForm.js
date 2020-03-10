49d87c3da97683fb07134e3d8ec02499acb7031c
github username <Lukabaramishvili@gmail.com>
2020-03-10 18:13:02 -0400

    merged from master

---
+++ client/components/newForm.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+import {Button, Checkbox, Form} from 'semantic-ui-react'
+
+class newForm extends Component {
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    )
+  }
+}
+
+export default newForm
