13b4ccb209da7b4bb2e6d1396dc85756bbc019f3
Kemi Thomas <kemothom71@gmail.com>
2020-03-21 21:43:35 -0400

    each input changes accordingly

--- client/components/userForm.js
+++ client/components/userForm.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchQuestions} from '../store/questions.js'
-import {Form, Button, Segment} from 'semantic-ui-react'
+import {Form, Button, Segment, Label} from 'semantic-ui-react'
 class userForm extends Component {
   constructor() {
     super()
     this.state = {
+      questions: [],
       value: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -16,37 +17,60 @@ class userForm extends Component {
     console.log('A name was submitted: ' + this.state.value)
     event.preventDefault()
   }
-
-  handleChange(e) {
-    e.preventDefault()
-    this.setState({
-      [event.target.name]: e.target.value
-    })
-  }
   async componentDidMount() {
     await this.props.fetchQuestions()
   }
+  handleChange(e, idx) {
+    e.preventDefault()
+    let newVal = this.state.value.slice(',')
+    this.setState({
+      newVal: e.target.value
+    })
+  }
+
   render() {
     console.log('PROPS', this.props)
     console.log('STATE', this.state)
     return (
       <div className="userForm">
         <Segment color="yellow">
-          <Form onSubmit={this.handleSubmit}>
+          <Form onSubmit={this.handleSubmit} onChange={this.handleChange}>
             {this.props.questions.map(q => {
+              return (
+                <label>
+                  {q.question}
+                  <input
+                    key={q.id}
+                    onChange={this.handleChange}
+                    name={q.question}
+                    type={q.type}
+                  />
+                </label>
+              )
+            })}
+            {/* {this.props.questions.map(question => {
+              return (
+                <Segment key={question.id}>
+                  <Label>{question.question}</Label>
+                </Segment>
+              )
+            })} */}
+            {/* {this.props.questions.map(q => {
               return (
                 <label key={q.id}>
                   {q.question}
                   <input
-                    name={q.type}
+                    name={q.question}
                     type={q.type}
                     value={this.state.value}
                     onChange={this.handleChange}
                   />
                 </label>
               )
-            })}
-            <Button type="submit" value="Submit" />
+            })} */}
+            <Button type="submit" value="Submit">
+              Submit
+            </Button>
           </Form>
         </Segment>
       </div>
